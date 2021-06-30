566304dbbe23169a30e9fc1e76e0d05adba1624b
Renjith Rajan <30775480+renjithrp@users.noreply.github.com>
2021-06-29 19:58:41 -0700

    Create actions.py

---
+++ actions.py
@@ -0,0 +1,34 @@
+#`````````keys``````````````````#
+#  spool_width
+#  spool_diaz
+#  wire_guage
+#  no_of_turns
+#  int_position
+#  turns_count
+#  pos_reach
+#  mode  (0 is Manual, 1 is automatic)
+#```````````````````````````````````````````#
+
+# Forward buttton action 
+def Forward(values):
+    print("Spool Width is : " + values["spool_width"])
+
+
+# Reverse buttton action 
+def Reverse(values):
+    print("Spool Width is : " + values["spool_width"])
+
+
+# Start buttton action
+def Start(values):
+    print("Spool Width is : " + values["spool_width"])
+
+
+# Start buttton action
+def Stop(values):
+    print("Spool Width is : " + values["spool_width"])
+
+
+# Pause buttton action
+def Pause(values):
+    print("Spool Width is : " + values["spool_width"])
