b8750890b4721608a7c83706c7e34b63c8bc66bb
Renjith Rajan <30775480+renjithrp@users.noreply.github.com>
2021-06-29 19:58:07 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+HEADER_TEXT = "SMART WINDER"
+WINDOW_SIZE = '850x560'
+BG_COLOR = "#009ACD"
+BUTTON_COLOR = "#00BFFF"
+ERROR_COLOR = "red"
+LABEL_FONT = "arial 12 normal"
+
+DATABASE_NAME = "smart-winder"
